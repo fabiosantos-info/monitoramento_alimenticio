e25003a8a34053b877b15630bad93400448640e8
Marcos Raia <79171007+MarcosRaia@users.noreply.github.com>
2024-11-09 14:17:02 +0000

    Commit3

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import logging
 from flask import Flask, request, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
-import logging
 import sqlite3
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 # Configuração de logs
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    filename='app.log',  # Nome do arquivo de log
+    level=logging.INFO,  # Nível de log (INFO, DEBUG, ERROR, etc.)
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Formato da mensagem de log
+)
 
 # Conexão com o banco de dados SQLite
 def get_db_connection():
@@ -17,12 +21,13 @@ def get_db_connection():
 
 @app.route('/')
 def index():
+    app.logger.info('Acessada a rota /')
     return 'Hello World'
-
 
 # Rotas CRUD
 @app.route('/alimentos', methods=['GET'])
 def get_alimentos():
+    app.logger.info('Acessada a rota /alimentos com método GET')
     conn = get_db_connection()
     alimentos = conn.execute('SELECT * FROM alimentos').fetchall()
     conn.close()
@@ -30,27 +35,40 @@ def get_alimentos():
 
 @app.route('/alimentos/tipo/<string:tipo>', methods=['GET'])
 def get_alimento_por_tipo(tipo):
+    app.logger.info(f'Acessada a rota /alimentos/tipo/{tipo} com método GET')
     conn = get_db_connection()
     alimentos = conn.execute('SELECT * FROM alimentos WHERE tipo = ?', (tipo,)).fetchall()
     conn.close()
-    return jsonify([dict(alimento) for alimento in alimentos]) if alimentos else ('', 404)
+    
+    if alimentos:
+        return jsonify([dict(alimento) for alimento in alimentos])
+    else:
+        return jsonify({"message": f"Nenhum alimento encontrado para o tipo '{tipo}'."}), 404
 
 @app.route('/alimentos', methods=['POST'])
 def create_alimento():
     novo_alimento = request.json
+    app.logger.info(f'Inserindo novo alimento: {novo_alimento}')
+    
     conn = get_db_connection()
     conn.execute('INSERT INTO alimentos (nome, tipo, cor, mes_colheita, categoria) VALUES (?, ?, ?, ?, ?)',
                  (novo_alimento['nome'], novo_alimento['tipo'], novo_alimento['cor'], novo_alimento['mes_colheita'], novo_alimento['categoria']))
+    
     conn.commit()
     conn.close()
+    
     return ('', 201)
 
 @app.route('/alimentos/<string:categoria>', methods=['DELETE'])
 def delete_alimento(categoria):
+    app.logger.info(f'Removendo alimentos da categoria: {categoria}')
+    
     conn = get_db_connection()
     conn.execute('DELETE FROM alimentos WHERE categoria = ?', (categoria,))
+    
     conn.commit()
     conn.close()
+    
     return ('', 204)
 
 if __name__ == '__main__':
